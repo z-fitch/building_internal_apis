cc9b8bbff1bb2602ec2fc377cf2f47b00e5d950f
Zanna Fitch <zannafitch2004@gmail.com>
2023-08-18 20:29:46 -0500

    create an API

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::BooksController < ApplicationController
+  def index
+    render json: Book.all
+  end
+
+  def show
+    render json: Book.find(params[:id])
+  end
+
+  def create
+    render json: Book.create(book_params)
+  end
+  
+  def update
+    render json: Book.update(params[:id], book_params)
+  end
+
+  def destroy
+    render json: Book.delete(params[:id])
+  end
+
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :author, :summary, :genre, :number_sold)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :books
+    end
+
+    namespace :v2 do
+      resources :books, only: [:index]
+    end
+  end
+end
